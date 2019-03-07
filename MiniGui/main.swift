cab1fe5b422b417964a20234566530e7c63bef28
Miguel de Icaza <miguel@gnome.org>
2019-03-06 21:58:33 -0800

    Initial Commit

---
+++ MiniGui/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiniGui
+//
+//  Created by Miguel de Icaza on 3/6/19.
+//  Copyright © 2019 Miguel de Icaza. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
