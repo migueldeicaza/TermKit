d950cc800641549af11efaf8650a52599a0e2449
miguel <miguel.de.icaza@gmail.com>
2019-05-20 22:26:25 -0400

    Small progress on TextView

--- TermKit/Views/TextView.swift
+++ TermKit/Views/TextView.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 typealias TextBuffer = TextField.TextBuffer
+typealias Position = Int64
+
 class TextModel {
     var lines: [TextBuffer]
     
@@ -76,15 +78,49 @@ class TextModel {
     }
     
     /// Adds a line to the model
-    func insert (_ line: TextBuffer, at: Int)
+    func insert (line: TextBuffer, at: Int)
     {
         lines.insert(line, at: at)
     }
     
-    func remove (at: Int)
+    func insert (text: TextBuffer, atLine: Int, andCol: Int)
+    {
+        lines [atLine].insert(contentsOf: text, at: andCol)
+    }
+    
+    /// Inserts the specified character at the given line and column
+    func insert (char: Character, atLine: Int, andCol: Int)
+    {
+        lines [atLine].insert((ch: char, size: Int8(char.cellSize())), at: andCol)
+    }
+    
+    func removeLine (at: Int)
     {
         lines.remove (at: at)
     }
+    
+    /// Encodes a column and row into a 64-bit position value, useful to compare ranges
+    static func toPosition (col: Int, row: Int) -> Position
+    {
+        return Int64 ((UInt32(row) << 32) | UInt32(col));
+    }
+    
+    static func fromPosition (_ position: Position) -> (col: Int, row: Int)
+    {
+        return (col: Int ((UInt32 (position) & 0xffffffff)), row: Int (position >> 32))
+    }
+    
+    func removeLineRange (atLine: Int, fromCol: Int, toCol: Int)
+    {
+        let lastCol = toCol == -1 ? lines [atLine].count : toCol
+        lines [atLine].removeSubrange (fromCol..<(lastCol-fromCol))
+    }
+    
+    func appendText (atLine: Int, txt: TextBuffer)
+    {
+        lines [atLine] += txt
+    }
+
 }
 
 /**
@@ -194,21 +230,10 @@ public class TextView : View {
         driver.setAttribute(hasFocus ? colorScheme!.focus : colorScheme!.normal)
     }
     
-    /// Encodes a column and row into a 64-bit value, useful to compare ranges
-    func encodeColRow (col: Int, row: Int) -> Int64
-    {
-        return Int64 ((UInt32(row) << 32) | UInt32(col));
-    }
-    
-    func decodeColRow (_ encoded: Int64) -> (col: Int, row: Int)
-    {
-        return (col: Int ((UInt32 (encoded) & 0xffffffff)), row: Int (encoded >> 32))
-    }
-    
     /// Returns an encoded region start..end (top 32 bits are the row, low32 the column) for the current selection
     func getEncodedRegionBounds () -> (start: Int64, end: Int64) {
-        let selection = encodeColRow (col: selectionStartColumn, row: selectionStartRow);
-        let point =  encodeColRow (col: currentColumn, row: currentRow)
+        let selection = TextModel.toPosition (col: selectionStartColumn, row: selectionStartRow);
+        let point =  TextModel.toPosition (col: currentColumn, row: currentRow)
         
         if (selection > point) {
             return (point, selection)
@@ -221,15 +246,15 @@ public class TextView : View {
     {
         let (start, end) = getEncodedRegionBounds()
         
-        let (sc, sr) = decodeColRow (start)
-        let (ec, er) = decodeColRow (end)
+        let (sc, sr) = TextModel.fromPosition (start)
+        let (ec, er) = TextModel.fromPosition (end)
         return (sc, sr, ec, er)
     }
     
     /// Returns true if the specified column and row are inside the selection
     func pointInSelection (col: Int, row: Int) -> Bool {
         let (start, end) = getEncodedRegionBounds()
-        let q = encodeColRow(col: col, row: row)
+        let q = TextModel.toPosition(col: col, row: row)
         return q >= start && q <= end
     }
     
@@ -257,19 +282,21 @@ public class TextView : View {
     func clearRegion ()
     {
         let (startCol, startRow, endCol, endRow) = getEncodedRegionCoords()
-        var line = model [Int(startCol)]
-        
+
         if startRow == endRow {
-            line.removeSubrange(Int(startCol)..<Int(endCol-startCol))
+            model.removeLineRange (atLine: startRow, fromCol: startCol, toCol: Int(endCol))
+            
             currentColumn = startCol
             setNeedsDisplay(Rect (x: 0, y: Int(startRow)-Int(topRow), width: frame.width, height: Int(startRow)-Int(topRow)+1))
             return
         }
-        line.removeSubrange(Int(startCol)..<(line.count-Int(startCol)))
+        model.removeLineRange(atLine: startRow, fromCol: startCol, toCol: -1)
         var line2 = model [Int(endRow)]
-        line += Array (line2 [Int(endCol)...])
+        
+        model.appendText (atLine: Int(endRow), txt: Array (line2 [Int(endCol)...]))
+        
         for row in (startRow+1)...endRow {
-            model.remove (at: Int (row))
+            model.removeLine (at: Int (row))
         }
         if currentColumn == endRow && currentRow == endRow {
             currentRow -= endRow - startRow
@@ -330,15 +357,192 @@ public class TextView : View {
     /// Inserts the provided character at the current cursor location
     func insert (char: Character)
     {
-        var line = getCurrentLine ()
-        line.insert((ch: char, size: Int8(char.cellSize())), at: currentColumn)
-        var prow = currentRow - topRow
+        model.insert(char: char, atLine: currentRow, andCol: currentColumn)
+        currentColumn += char.cellSize()
+        let prow = currentRow - topRow
         setNeedsDisplay (Rect(x: 0, y: prow, width: frame.width, height: prow+1))
     }
     
     /// Inserts the provided string at the current cursor location
     func insertText(text: String)
     {
+        var lines = TextModel.stringToTextBufferArray(text: text)
+        if lines.count == 0 {
+            return
+        }
+        
+        // Optimize single line
+        if lines.count == 1 {
+            model.insert(text: lines [0], atLine: currentRow, andCol: currentColumn)
+            currentColumn += text.cellCount()
+            if currentColumn - leftColumn > frame.width {
+                leftColumn = currentColumn - frame.width - 1
+            }
+            setNeedsDisplay(Rect (x: 0, y: currentRow-topRow, width: frame.width, height: currentRow-topRow+1))
+            return
+        }
+        // Keep a copy of the rest of the line
+        let line = getCurrentLine()
+        let rest = Array (line [currentColumn...])
+        model.removeLineRange(atLine: currentRow, fromCol: currentColumn, toCol: line.count)
+        
+        // first line is inserted at the cursor location, the rest is appended
+        model.insert(text: lines [0], atLine: currentRow, andCol: currentColumn)
+        for i in 1..<lines.count {
+            model.insert(line: lines [i], at: currentRow+i)
+        }
+        
+        let last = model [currentRow+lines.count-1]
+        let lastp = last.count
+        model.insert (text: rest, atLine: currentRow+lines.count-1, andCol: lastp)
+        
+        // Now adjust column and line positions
+        currentRow += lines.count + 1
+        var column = 0
+        for i in 0..<lastp {
+            column += Int (last [i].size)
+        }
+        currentColumn = column
+        if currentColumn < leftColumn {
+            leftColumn = currentColumn
+        }
+        if currentColumn - leftColumn >= frame.width {
+            leftColumn = currentColumn - frame.width + 1
+        }
+        setNeedsDisplay()
+    }
     
+    // The column we are tracking, or -1 if we are not tracking any column
+    var columnTrack : Int = -1
+    
+    /// Tries to snap the cursor to the tracking column
+    func trackColumn ()
+    {
+        // Now track the column
+        let line = getCurrentLine()
+        if line.count < columnTrack {
+            currentColumn = line.count
+        } else if columnTrack != -1 {
+            currentColumn = columnTrack
+        } else if currentColumn > line.count {
+            currentColumn = line.count
+        }
+        adjust ()
+    }
+    
+    func adjust ()
+    {
+        var need = false
+        if currentColumn < leftColumn {
+            currentColumn = leftColumn
+            need = true
+        }
+        if currentColumn - leftColumn > frame.width {
+            leftColumn = currentColumn - frame.width + 1
+            need = true
+        }
+        if currentRow < topRow {
+            topRow = currentRow
+            need = true
+        }
+        if currentRow - topRow > frame.height {
+            topRow = currentRow - frame.height + 1
+            need = true
+        }
+        if need {
+            setNeedsDisplay()
+        } else {
+            positionCursor()
+        }
+    }
+    
+    /**
+     * Will scroll the view to display the specified row at the top
+     * - Parameter row: Row that should be displayed at the top, if the value is negative it will be reset to zero
+     */
+    public func scrollTo (row: Int)
+    {
+        let rrow = row < 0 ? 0 : row
+        topRow = rrow > model.count ? model.count-1 : rrow
+        setNeedsDisplay()
+    }
+    
+    var lastWasKill = false
+    
+    public override func processKey(event: KeyEvent) -> Bool {
+        // Handle some state here - whether the last command was a kill
+        // operation and the column tracking (up/down)
+        switch event.key {
+        case .ControlN, .CursorDown, .ControlP, .CursorUp:
+            lastWasKill = false
+        case .ControlK:
+            break
+        default:
+            lastWasKill = false
+            columnTrack = -1
+        }
+        
+        // Dispatch the command
+        switch event.key {
+        case .PageDown, .ControlV:
+            let nPageDnShift = frame.height - 1
+            if currentRow < model.count {
+                if columnTrack == -1 {
+                    columnTrack = currentColumn
+                }
+                currentRow = (currentRow + nPageDnShift) > model.count ? model.count : currentRow + nPageDnShift
+                if topRow < currentRow - nPageDnShift {
+                    topRow = currentRow >= model.count ? currentRow - nPageDnShift : topRow + nPageDnShift
+                    setNeedsDisplay()
+                }
+                trackColumn()
+                // positionCursor()
+            }
+        case .PageUp, .Letter ("v") where event.isAlt:
+            let nPageUpShift = frame.height - 1;
+            if currentRow > 0 {
+                if columnTrack == -1 {
+                    columnTrack = currentColumn;
+                }
+                currentRow = currentRow - nPageUpShift < 0 ? 0 : currentRow - nPageUpShift;
+                if currentRow < topRow {
+                    topRow = topRow - nPageUpShift < 0 ? 0 : topRow - nPageUpShift;
+                    setNeedsDisplay ();
+                }
+                trackColumn ();
+                // PositionCursor ();
+            }
+        case .ControlN, .CursorDown:
+            if currentRow + 1 < model.count {
+                if columnTrack == -1 {
+                    columnTrack = currentColumn
+                }
+                currentRow += 1
+                if currentRow >= topRow + frame.height {
+                    topRow += 1
+                    setNeedsDisplay ()
+                }
+                trackColumn ();
+                // positionCursor ();
+            }
+        case .ControlP, .CursorUp:
+            if currentRow > 0 {
+                if columnTrack == -1 {
+                    columnTrack = currentColumn
+                }
+                currentRow -= 1
+                if currentRow < topRow {
+                    topRow -= 1
+                    setNeedsDisplay ()
+                }
+                trackColumn ();
+                // positionCursor ();
+            }
+            
+        // Next up: ControlF
+        default:
+            break
+        }
+        return true
     }
 }
