2592b34df314f2dd1cf6063f362ac3428ee246df
Miguel de Icaza <miguel@gnome.org>
2025-08-08 22:22:30 -0400

    Workaround for Swift on Linux crash

--- Tests/TermKitTests/XCTestManifests.swift
+++ Tests/TermKitTests/XCTestManifests.swift
@@ -1,6 +1,7 @@
 import XCTest
 
-#if !canImport(ObjectiveC)
+// Workaround for crash in SwiftLinux
+#if !canImport(ObjectiveC) && !os(Linux)
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(TermKitTests.allTests),
